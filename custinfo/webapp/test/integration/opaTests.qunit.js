e01b3082f2e87bdd6767330b9ff7daa724c1b8f0
wlduseom <happy082798@gmail.com>
2022-11-03 18:01:11 +0900

    first commit

---
+++ custinfo/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sync/sap/custinfo/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ custinfo/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"syncsap/custinfo/test/unit/controller/Master.controller"
+], function () {
+	"use strict";
+});
